cd24cd8395682c907ae804cc62b248085269439e
apossstle <apostlebehelite@gmail.com>
2026-04-20 15:59:17 +0500

    Refactor server startup to use environment variable for port

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import argparse
+#ЗАПУСК ПО ЛОКАЛКЕ------------------------------------------------------------------------------------
+
+
+# import argparse
+# import sys
+# import os
+
+# sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+# from api.server import run_server
+
+
+# def main():
+#     parser = argparse.ArgumentParser(description="Polygon Service")
+#     parser.add_argument("--host",      default="127.0.0.1")
+#     parser.add_argument("--port",      type=int, default=8080)
+#     parser.add_argument("--cell-size", type=float, default=1.0, dest="cell_size",
+#                         help="Размер ячейки пространственного индекса")
+#     args = parser.parse_args()
+#     run_server(host=args.host, port=args.port)
+
+
+# if __name__ == "__main__":
+#     main()
+
+#ЗАПУСК ЧЕРЕЗ СЕРВЕР------------------------------------------------------------------------------------------------
+
 import sys
 import os
 
@@ -6,16 +32,6 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from api.server import run_server
 
-
-def main():
-    parser = argparse.ArgumentParser(description="Polygon Service")
-    parser.add_argument("--host",      default="127.0.0.1")
-    parser.add_argument("--port",      type=int, default=8080)
-    parser.add_argument("--cell-size", type=float, default=1.0, dest="cell_size",
-                        help="Размер ячейки пространственного индекса")
-    args = parser.parse_args()
-    run_server(host=args.host, port=args.port)
-
-
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 8080))
+    run_server(host="0.0.0.0", port=port)
